8f3e264acd05764fab4f7853c8b6e09f03486797
Rianne Pada <rpada@hawaii.edu>
2022-12-15 13:32:55 -1000

    updates

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+const elebutton = document.getElementById("grid-button")
+elebutton.addEventListener("click", newGrid);
+
+const rainbowButton = document.getElementById("rainbow-button")
+rainbowButton.addEventListener("click", colorGen);
+
+const clearButton = document.getElementById("clear-button")
+clearButton.addEventListener("click", clearOriginal);
 
 function setupGrid(){
     const container = document.querySelector('#Grid');
@@ -17,12 +25,37 @@ for (i=0; i < 256; i++){
   }
 }
 
-const elebutton = document.getElementById("grid-button")
-elebutton.addEventListener("click", newGrid);
+function clearOriginal(){
+    const container = document.querySelector('#Grid');
+    container.textContent = ""
+    document.getElementById("Grid").style.gridTemplateColumns = "repeat(16, auto)"; // https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/
+    boxes()
+}
+
+function colorGen() {
+    const container = document.querySelector('#Grid');
+    container.textContent = ""
+    document.getElementById("Grid").style.gridTemplateColumns = "repeat(16, auto)"; // https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/
+    for (i=0; i < 256; i++){
+
+    const container = document.querySelector('#Grid');
+    const squarediv = document.createElement('div');
+    squarediv.classList.add('squarediv');    
+    squarediv.setAttribute('id', 'squarediv'); 
+    squarediv.addEventListener("mouseover", function(event) {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        squarediv.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    })
+    container.appendChild(squarediv);
+}
+}
 
 function newGrid(){
-       // ask the user new value for the length of one side of the square
+    // creating actual grid
     let userChoice = prompt("How many boxes on one side would you like?")
+       // ask the user new value for the length of one side of the square
     if (userChoice > 100 || userChoice <= 0 || isNaN(userChoice)) {
         alert("Please enter a valid number (less than 100, greater than zero, not a letter or special character.)")
     } else {
@@ -35,5 +68,43 @@ function newGrid(){
         setupGrid()
       }
     }
+
+    // clear button WHEN NEW GRID BUTTON IS PUSHED
+    function clearNew(){
+        const container = document.querySelector('#Grid');
+        container.textContent = ""
+        document.getElementById("Grid").style.gridTemplateColumns = `repeat(${userChoice}, auto)`; // https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/
+        const oneSide = Math.pow(userChoice, 2)
+        for (i=0; i < oneSide; i++){
+            setupGrid()
+           }
+    }
+    const clearButton = document.getElementById("clear-button")
+    clearButton.addEventListener("click", clearNew);   
+
+    // rainbow button WHEN NEW GRID BUTTON IS PUSHED
+    function rainbow(){
+        const container = document.querySelector('#Grid');
+        container.textContent = ""
+        document.getElementById("Grid").style.gridTemplateColumns = `repeat(${userChoice}, auto)`; // https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/
+        const oneSide = Math.pow(userChoice, 2)
+        for (i=0; i < oneSide; i++){
+        const container = document.querySelector('#Grid');
+        const squarediv = document.createElement('div');
+        squarediv.classList.add('squarediv');    
+        squarediv.setAttribute('id', 'squarediv'); 
+        squarediv.addEventListener("mouseover", function(event) {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            squarediv.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+        })
+        container.appendChild(squarediv);
+    }
+    }
+    const rainbowButton = document.getElementById("rainbow-button")
+    rainbowButton.addEventListener("click", rainbow);
+
+    return userChoice
 }
-boxes()
+ boxes()
